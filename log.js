e1aa6921c7b11d86622ec7074e28795369a42758
Dilshan Jayasekara <130susith@gmail.com>
2021-03-14 00:44:39 +0530

    Second Commit

--- log.js
+++ log.js
@@ -1,3 +1,8 @@
 function logWarningMessage(message){
     console.log("WARNING : "+ message)
 }
+function logErrorMessage(message){
+    console.log("WARNING : "+ message)
+}
+logWarningMessage("print is out of Ink")
+logErrorMessage("Building is on fire")
