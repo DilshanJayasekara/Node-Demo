22ff6ef0b5d309cea2b15b3966b73a683341d977
DilshanJayasekara <130susith@gmail.com>
2021-03-17 21:51:01 +0530

    Update 3rd Video

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var logger = require("./log");
-console.log(logger.logErrorMessage("Building is on fire!"));
+//console.log(logger.logErrorMessage("Building is on fire!"));
 console.log(logger.logWarningMessage("Building is on fire Warning!"));
--- log.js
+++ log.js
@@ -1,10 +1,20 @@
-function logWarningMessage(message){
-    return ("WARNING : "+ message)
-}
-function logErrorMessage(message){
-     return ("Error : "+ message)
-}
+//var logWarningMessage = function(message){
+ //   return ("WARNING : "+ message)
+//};
+//var logErrorMessage = function (message){
+//     return ("Error : "+ message)
+//};
+
+var logModule = {
+    logWarningMessage : function(message){
+        return ("WARNING : "+ message)
+    },
+    logErrorMessage : function (message){
+        return ("Error : "+ message)
+   }
+};
 //logWarningMessage("print is out of Ink")
 //logErrorMessage("Building is on fire")
-module.exports.logErrorMessage = logErrorMessage;
-module.exports.logWarningMessage = logWarningMessage;
+//module.exports.logErrorMessage = logErrorMessage;
+//module.exports.logWarningMessage = logWarningMessage;
+module.exports = logModule;
